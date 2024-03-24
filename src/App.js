246550acb3e6481aa00d1e5eed50c48f63e52ecc
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-03-24 17:42:13 +0300

    Add async posts with redux-thunk.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import FetchedPosts from "./components/FetchedPosts";
 import PostForm from "./components/PostForm";
 import Posts from "./components/Posts";
 
-const defaultPosts = [
-  { id: 0, title: "Post 1", text: "Info 1" },
-  { id: 1, title: "Post 2", text: "Info 2" },
-  { id: 2, title: "Post 3", text: "Info 3" },
-];
-
 function App() {
   return (
     <div className="container">
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { compose, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import { rootReducer } from "./redux/rootReducer";
 import { Provider } from "react-redux";
+import { thunk } from "redux-thunk";
 
 const store = createStore(
   rootReducer,
   compose(
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
